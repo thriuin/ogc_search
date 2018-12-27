cf20181d0213a77e484cd5de218a916b59f47335
thriuin <ross.thompson@outlook.com>
2018-12-27 15:49:49 -0500

    Update load file to work on Windows

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # ogc_search
 Django based search for the Canadian Open Government Portal 
 
-This project uses a Django framework web application as a thin frontend to Solr to do searching of datasets and proactive disclosure data 
-for the Open Government Portal. Instead of using the usual CKAN Solr 6 cores, data is loaded into custom Solr cores that better support 
-Canada's two official languages.
+This project uses a Django framework web application as a thin frontend to Solr to do searching of datasets and 
+proactive disclosure data for the Open Government Portal. Instead of using the usual CKAN Solr 6 cores, data is 
+loaded into custom Solr cores that better support Canada's two official languages.
+
+## Setup
+
+Some extra steps are required
+
+Download https://raw.githubusercontent.com/open-data/ckanext-canada/master/ckanext/canada/schemas/presets.yaml
+to the ckan older 
+Download a copy of the WET GCWEB theme files (available at 
+http://wet-boew.github.io/wet-boew/docs/versions/dwnld-en.html) to the themes-dist-GCWeb folder
+
+## Installing on Solr 7.6:
+- Create a new core using `solr -c <core_name> create`
+- In the conf folder remove the managed-schema file and copy the schema-7.6.xml file to schema. Rename it to schema.xml.
+- Copy the elevate.xml, synonyms_en.txt, and synonyms_fr.xml to the conf folder 
+- Copy the solrconfig-7.6.xml file to conf/solrconfig.xml
--- ogc_search/load_od_to_solr.py
+++ ogc_search/load_od_to_solr.py
@@ -1,10 +1,10 @@
-
 from django.conf import settings
 import os
 import pysolr
 import simplejson as json
 import sys
 from yaml import load
+
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
@@ -13,10 +13,11 @@ except ImportError:
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ogc_search.settings')
 
 ckan_schema_presets = {}
-with open(settings.CKAN_YAML_FILE, encoding='utf-8-sig') as ckan_schema_file:
+with open(settings.CKAN_YAML_FILE, mode='r', encoding='utf8', errors="ignore") as ckan_schema_file:
     ckan_schema_presets = load(ckan_schema_file, Loader=Loader)
 
-def get_cs_choices(field_name, lang = 'en'):
+
+def get_cs_choices(field_name, lang='en'):
     choices_en = {}
     choices_fr = {}
 
@@ -31,6 +32,7 @@ def get_cs_choices(field_name, lang = 'en'):
 
     return {'en': choices_en, 'fr': choices_fr}
 
+
 controlled_lists = {}
 
 controlled_lists['subject'] = get_cs_choices('canada_subject')
@@ -41,97 +43,103 @@ controlled_lists['subject'] = get_cs_choices('canada_subject')
 controlled_lists['resource_type'] = get_cs_choices('canada_resource_type')
 controlled_lists['frequency'] = get_cs_choices('canada_frequency')
 
-solr = pysolr.Solr('http://127.0.0.1:8983/solr/core_od_search')
+solr = pysolr.Solr(settings.SOLR_URL)
 solr.delete(q='*:*')
 solr.commit()
 
-with open(sys.argv[1], 'r') as j:
+with open(sys.argv[1], 'r', encoding='utf8', errors="ignore") as j:
     od_list = []
     bulk_size = 500
     i = 0
     total = 0
     for jl in j:
-        o = json.loads(jl)
-        owner_org_titles = str(o['organization']['title']).split('|')
-        owner_org_title_en = owner_org_titles[0]
-        owner_org_title_fr: str = owner_org_titles[1]
-        subjects_en = []
-        subjects_fr =[]
-        for s in o['subject']:
-            subjects_en.append(controlled_lists['subject']['en'][s])
-            subjects_fr.append(controlled_lists['subject']['fr'][s])
-        resource_type_en = []
-        resource_type_fr = []
-        resource_fmt = []
-        resource_title_en = []
-        resource_title_fr = []
+        try:
+            o = json.loads(jl)
+            owner_org_titles = str(o['organization']['title']).split('|')
+            owner_org_title_en = owner_org_titles[0]
+            owner_org_title_fr: str = owner_org_titles[1]
+            subjects_en = []
+            subjects_fr = []
+            for s in o['subject']:
+                subjects_en.append(controlled_lists['subject']['en'][s])
+                subjects_fr.append(controlled_lists['subject']['fr'][s])
+            resource_type_en = []
+            resource_type_fr = []
+            resource_fmt = []
+            resource_title_en = []
+            resource_title_fr = []
 
-        for r in o['resources']:
-            resource_type_en.append(
-                controlled_lists['resource_type']['en'][r['resource_type']]
-                if r['resource_type'] in controlled_lists['resource_type']['en'] else '')
-            resource_type_fr.append(
-                controlled_lists['resource_type']['fr'][r['resource_type']]
-                if r['resource_type'] in controlled_lists['resource_type']['fr'] else '')
-            resource_fmt.append(r['format'])
+            for r in o['resources']:
+                resource_type_en.append(
+                    controlled_lists['resource_type']['en'][r['resource_type']]
+                    if r['resource_type'] in controlled_lists['resource_type']['en'] else '')
+                resource_type_fr.append(
+                    controlled_lists['resource_type']['fr'][r['resource_type']]
+                    if r['resource_type'] in controlled_lists['resource_type']['fr'] else '')
+                resource_fmt.append(r['format'])
 
-            if 'en' in r['name_translated']:
-                resource_title_en.append(r['name_translated']['en'])
-            elif 'fr-t-en'in r['name_translated']:
-                resource_title_en.append(r['name_translated']['fr-t-en'])
-            if 'fr' in r['name_translated']:
-                resource_title_fr.append(r['name_translated']['fr'].strip())
-            elif 'en-t-fr' in r['name_translated']:
-                resource_title_fr.append(r['name_translated']['en-t-fr'].strip())
+                if 'en' in r['name_translated']:
+                    resource_title_en.append(r['name_translated']['en'])
+                elif 'fr-t-en' in r['name_translated']:
+                    resource_title_en.append(r['name_translated']['fr-t-en'])
+                if 'fr' in r['name_translated']:
+                    resource_title_fr.append(r['name_translated']['fr'].strip())
+                elif 'en-t-fr' in r['name_translated']:
+                    resource_title_fr.append(r['name_translated']['en-t-fr'].strip())
 
-        od_obj = {
-            'portal_type_en_s': controlled_lists['type']['en'][o['type']],
-            'portal_type_fr_s': controlled_lists['type']['fr'][o['type']],
-            'collection_type_en_s': controlled_lists['collection']['en'][o['collection']],
-            'collection_type_fr_s': controlled_lists['collection']['fr'][o['collection']],
-            'jurisdiction_en_s': controlled_lists['jurisdiction']['en'][o['jurisdiction']],
-            'jurisdiction_fr_s': controlled_lists['jurisdiction']['fr'][o['jurisdiction']],
-            'owner_org_title_en_s': owner_org_title_en,
-            'owner_org_title_fr_s': owner_org_title_fr,
-            'subject_en_s': subjects_en,
-            'subject_fr_s': subjects_fr,
-            'resource_type_en_s': list(set(resource_type_en)),
-            'resource_type_fr_s': list(set(resource_type_fr)),
-            'update_cycle_en_s': controlled_lists['frequency']['en'][o['frequency']],
-            'update_cycle_fr_s': controlled_lists['frequency']['fr'][o['frequency']],
-            'id_name_s': o['name'],
-            'owner_org_s': o['organization']['name'],
-            'author_txt': '' if o['author'] is None else o['author'],
-            'description_txt_en': o['notes_translated']['en'] if 'en' in o['notes_translated'] else '',
-            'description_txt_fr': o['notes_translated']['fr'] if 'fr' in o['notes_translated'] else '',
-            'description_xlt_txt_en': o['notes_translated']['fr-t-en'] if 'fr-t-en' in o['notes_translated'] else '',
-            'description_xlt_txt_fr': o['notes_translated']['en-t-fr'] if 'en-t-f-r' in o['notes_translated'] else '',
-            'title_en_s': str(o['title_translated']['en']).strip() if 'en' in o['title_translated'] else '',
-            'title_fr_s': str(o['title_translated']['fr']).strip() if 'fr' in o['title_translated'] else '',
-            'title_xlt_en_s': o['title_translated']['fr-t-en'] if 'fr-t-en' in o['title_translated'] else '',
-            'title_xlt_fr_s': o['title_translated']['en-t-fr'] if 'en-t-fr' in o['title_translated'] else '',
-            'resource_format_s': list(set(resource_fmt)),
-            'resource_title_en_s': resource_title_en,
-            'resource_title_fr_s': resource_title_fr,
-            'last_modified_tdt': o['metadata_modified'] + 'Z'
-        }
-        if 'en' in o['keywords']:
-            od_obj['keywords_en_s'] = o['keywords']['en']
-        elif 'fr-t-en' in o['keywords']:
-            od_obj['keywords_en_s'] = o['keywords']['fr-t-en']
-        if 'fr' in o['keywords']:
-            od_obj['keywords_fr_s'] = o['keywords']['fr']
-        elif 'en-t-fr' in o['keywords']:
-            od_obj['keywords_fr_s'] = o['keywords']['en-t-fr']
-        od_list.append(od_obj)
-        i += 1
-        if i == bulk_size:
-            solr.add(od_list)
-            solr.commit()
-            od_list = []
-            total += i
-            print('{0} Records Processed'.format(total))
-            i = 0
+            od_obj = {
+                'portal_type_en_s': controlled_lists['type']['en'][o['type']],
+                'portal_type_fr_s': controlled_lists['type']['fr'][o['type']],
+                'collection_type_en_s': controlled_lists['collection']['en'][o['collection']],
+                'collection_type_fr_s': controlled_lists['collection']['fr'][o['collection']],
+                'jurisdiction_en_s': controlled_lists['jurisdiction']['en'][o['jurisdiction']],
+                'jurisdiction_fr_s': controlled_lists['jurisdiction']['fr'][o['jurisdiction']],
+                'owner_org_title_en_s': owner_org_title_en,
+                'owner_org_title_fr_s': owner_org_title_fr,
+                'subject_en_s': subjects_en,
+                'subject_fr_s': subjects_fr,
+                'resource_type_en_s': list(set(resource_type_en)),
+                'resource_type_fr_s': list(set(resource_type_fr)),
+                'update_cycle_en_s': controlled_lists['frequency']['en'][o['frequency']],
+                'update_cycle_fr_s': controlled_lists['frequency']['fr'][o['frequency']],
+                'id_name_s': o['name'],
+                'owner_org_s': o['organization']['name'],
+                'author_txt': '' if o['author'] is None else o['author'],
+                'description_txt_en': o['notes_translated']['en'] if 'en' in o['notes_translated'] else '',
+                'description_txt_fr': o['notes_translated']['fr'] if 'fr' in o['notes_translated'] else '',
+                'description_xlt_txt_en': o['notes_translated']['fr-t-en'] if 'fr-t-en' in o[
+                    'notes_translated'] else '',
+                'description_xlt_txt_fr': o['notes_translated']['en-t-fr'] if 'en-t-f-r' in o[
+                    'notes_translated'] else '',
+                'title_en_s': str(o['title_translated']['en']).strip() if 'en' in o['title_translated'] else '',
+                'title_fr_s': str(o['title_translated']['fr']).strip() if 'fr' in o['title_translated'] else '',
+                'title_xlt_en_s': o['title_translated']['fr-t-en'] if 'fr-t-en' in o['title_translated'] else '',
+                'title_xlt_fr_s': o['title_translated']['en-t-fr'] if 'en-t-fr' in o['title_translated'] else '',
+                'resource_format_s': list(set(resource_fmt)),
+                'resource_title_en_s': resource_title_en,
+                'resource_title_fr_s': resource_title_fr,
+                'last_modified_tdt': o['metadata_modified'] + 'Z'
+            }
+            if 'en' in o['keywords']:
+                od_obj['keywords_en_s'] = o['keywords']['en']
+            elif 'fr-t-en' in o['keywords']:
+                od_obj['keywords_en_s'] = o['keywords']['fr-t-en']
+            if 'fr' in o['keywords']:
+                od_obj['keywords_fr_s'] = o['keywords']['fr']
+            elif 'en-t-fr' in o['keywords']:
+                od_obj['keywords_fr_s'] = o['keywords']['en-t-fr']
+            od_list.append(od_obj)
+            i += 1
+            if i == bulk_size:
+                solr.add(od_list)
+                solr.commit()
+                od_list = []
+                total += i
+                print('{0} Records Processed'.format(total))
+                i = 0
+        except Exception as x:
+            print('Error on line {0}: {1}'.format(i, x))
+            pass
 
     if len(od_list) > 0:
         solr.add(od_list)
