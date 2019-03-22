7bdbb48e1291383a4f73b759289f214d0b1e979b
thriuin <ross.thompson@tbs-sct.gc.ca>
2019-03-22 13:06:19 -0400

    Use a separate log just for query logging

--- ogc_search/ogc_search/middleware.py
+++ ogc_search/ogc_search/middleware.py
@@ -7,7 +7,7 @@ class QueryLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
-        self.logger = logging.getLogger('ogc_search')
+        self.logger = logging.getLogger('ogc_query')
 
     def __call__(self, request):
         # Code to be executed for each request before
