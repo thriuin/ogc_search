8e18e995bd82a16756e15dee9f6e121041237ba6
thriuin <ross.thompson@tbs-sct.gc.ca>
2019-07-16 16:18:05 -0400

    Address Django security issue

--- ogc_search/clean_export_cache.py
+++ ogc_search/clean_export_cache.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ogc_search.settings')
 
-clean_cmd = "find {0} -name '*.csv' -type f -mmin +30 exec rm {{}}\;".format(settings.EXPORT_FILE_CACHE_DIR)
+clean_cmd = "find {0} -name '*.csv' -type f -mmin +30 -exec rm '{{}}' \;".format(settings.EXPORT_FILE_CACHE_DIR)
 os.system(clean_cmd)
