c120a859a4e9cc299ba9e8ec8a522d3730401040
thriuin <ross.thompson@tbs-sct.gc.ca>
2019-11-07 16:00:36 -0500

    Fix for nothing to report facet

--- ogc_search/contracts/static/facets.js
+++ ogc_search/contracts/static/facets.js
@@ -1,2 +1,2 @@
 let accumulators = ['ct-search-orgs', 'ct-search-year', 'ct-search-commodity-type', 'ct-search-country',
-    'ct-search-agreement', 'ct-search-dollar-range', 'ct-solicitation', 'ct-search-doc'];
+    'ct-search-agreement', 'ct-search-dollar-range', 'ct-solicitation', 'ct-search-doc', 'ct-search-report-type'];
