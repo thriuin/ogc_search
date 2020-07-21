9e775b91ac6c75c2be694a976c8647eab8e092cf
thriuin <ross.thompson@tbs-sct.gc.ca>
2020-07-21 14:48:49 -0400

    GC search record fix pt2

--- ogc_search/open_data/static/od_facet_buttons.js
+++ ogc_search/open_data/static/od_facet_buttons.js
@@ -15,9 +15,11 @@ $(function(){
     let facets = [];
     if (window.location.search.length > 1) {
         facets = search_terms.substring(1).split('&');
+
         for (let i=0; i<facets.length; i++) {
             let pair = facets[i].split('=');
             sessionStorage.setItem(pair[0], pair[1]);
+            /* temp fix
             if (pair[0].substring(0,3).match(/^(\D\D-)$/)) {
                 let display_terms = decodeURIComponent(pair[1]).split('|');
                 for(let j=0; j<display_terms.length; j++) {
@@ -34,7 +36,9 @@ $(function(){
                     $('#search_terms').append(new_link).append(' ');
                 }
             }
+            */
         }
-        $('#page').val('1');
+
     }
+ $('#page').val('1');
 });
