9ae03239ca88a4efa9b82bfe7db577ec40e14403
thriuin <ross.thompson@tbs-sct.gc.ca>
2020-10-05 11:31:05 -0400

    OPN 489: Update function call for GC search results download

--- ogc_search/grants/views.py
+++ ogc_search/grants/views.py
@@ -501,8 +501,7 @@ class GCExportView(View):
                                                            facets_dict,
                                                            self.phrase_xtras)
 
-        if search_util.cache_search_results_file(cached_filename=cached_filename, sr=search_results,
-                                                 solr_fields=solr_fields):
+        if search_util.cache_search_results_file(cached_filename=cached_filename, sr=search_results):
             if settings.EXPORT_FILE_CACHE_URL == "":
                 return FileResponse(open(cached_filename, 'rb'), as_attachment=True)
             else:
